becf3f01ab6b2cbf4e7a30d208378e9ae510f4d0
Mauri2392 <mauriciohalberg@gmail.com>
2024-12-25 03:23:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+let slider = document.querySelector('.slider .list');
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let dots = document.querySelectorAll('.slider .dots li');
+
+let lengthItems = items.length - 1;
+let active = 0;
+next.onclick = function(){
+    active = active + 1 <= lengthItems ? active + 1 : 0;
+    reloadSlider();
+}
+prev.onclick = function(){
+    active = active - 1 >= 0 ? active - 1 : lengthItems;
+    reloadSlider();
+}
+let refreshInterval = setInterval(()=> {next.click()}, 3000);
+function reloadSlider(){
+    slider.style.left = -items[active].offsetLeft + 'px';
+    // 
+    let last_active_dot = document.querySelector('.slider .dots li.active');
+    last_active_dot.classList.remove('active');
+    dots[active].classList.add('active');
+
+    clearInterval(refreshInterval);
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
+}
+
+dots.forEach((li, key) => {
+    li.addEventListener('click', ()=>{
+         active = key;
+         reloadSlider();
+    })
+})
+window.onresize = function(event) {
+    reloadSlider();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.createElement('button');
+    button.textContent = "Click acá";
+    button.style.marginTop = "20px";
+    document.body.appendChild(button);
+
+    button.addEventListener('click', () => {
+        alert("FELICITACIONES GONZA!!! TE QUEREMOS MUCHO 🥰 MARI Y PETA");
+    });
+}); 
+
+var swiper = new Swiper(".gallery-slider", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 20,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    loop:true,
+    autoplay:{
+      delay: 3000,
+      disableOnInteraction:false,
+    }
+  }); 
